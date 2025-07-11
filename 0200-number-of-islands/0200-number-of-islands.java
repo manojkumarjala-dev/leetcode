52c334a27c78f100791f6660253a270421f274b7
Manoj Kumar Jala <106336600+manojkumarjala-dev@users.noreply.github.com>
2025-07-11 17:13:46 -0400

    Time: 11 ms (7.79%), Space: 50.6 MB (27.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,52 +1,42 @@
 class Solution {
-    private int n;
-    private int m;
-    private int[][] visited;
-    private char[][] matrix;
-    public boolean isValid(int row, int col){
-        return (row>=0 && row<n && col>=0 && col<m);
-    }
-    public void bfs(int row, int col){
-        int[][] dir = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+
+    public int numIslands(char[][] grid) {
+
+        int[][] visited = new int[grid.length][grid[0].length];      
+        int count = 0;
         Queue<int[]> q = new LinkedList<>();
 
-        q.offer(new int[]{row, col});
-
-        while(q.size()>0){
-            int size = q.size();
-            while(size-->0){
-                int[] cur_pos = q.poll();
-                for(int[] d: dir){
-                    int new_row = cur_pos[0]+d[0];
-                    int new_col = cur_pos[1]+d[1];
-                    if(isValid(new_row, new_col) && visited[new_row][new_col]==0 && matrix[new_row][new_col]=='1'){
-                        visited[new_row][new_col]=1;
-                        q.offer(new int[]{new_row,new_col});
-                    }
-                    
-                }
-            }
-        }
-        
-    }
-    public int numIslands(char[][] grid) {
-        n = grid.length;
-        m = grid[0].length;
-        visited = new int[n][m];
-        matrix = grid;
-        int ans = 0;
-
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(grid[i][j]=='1' && visited[i][j]==0){
+        for(int i =0;i<grid.length;i++){
+            for(int j = 0;j<grid[0].length;j++){
+                if(visited[i][j]==0 && grid[i][j]=='1'){
+                    count++;
                     visited[i][j] = 1;
-                    bfs(i,j);
-                    ans++;
-                }
-            }
-        }
+                    q.offer(new int[]{i,j});
+                    while(q.size()>0){
+                        int size = q.size();
+                        while(size-->0){
+                            int[] pos = q.poll();
+                            int[][] directions = {{1,0},{0,1},{-1,0},{0,-1}};
 
-        return ans;
-        
+                            for(int[] dir: directions){
+                                int new_row = dir[0] + pos[0];
+                                int new_col = dir[1] + pos[1];
+
+                                if(new_row>=0 && new_col>=0 && new_row<grid.length && new_col<grid[0].length){
+                                    if(visited[new_row][new_col]==0 && grid[new_row][new_col]=='1'){
+                                        visited[new_row][new_col] = 1;
+                                        q.offer(new int[]{new_row, new_col});
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+            }
+        }  
+
+        return count;
     }
 }
