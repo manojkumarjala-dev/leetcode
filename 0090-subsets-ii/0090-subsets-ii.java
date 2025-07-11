68e8e424afb8e9cd59ec7cae2fd7651eca39aaaf
Manoj Kumar Jala <106336600+manojkumarjala-dev@users.noreply.github.com>
2025-07-11 14:19:35 -0400

    Time: 1 ms (99.93%), Space: 43.5 MB (61.17%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,4 +1,9 @@
 class Solution {
+    // wacth neetcode subsetrs 2 video fro reference
+    // core idea is instead of thinkin take and not take 
+    // consider if i take a value then all the children must include what is there
+
+    // if i dont take a node then i should skip all the nodes which have same value as the node i just skipped
     void helper(int[] nums, List<List<Integer>> ans, int idx, List<Integer> current){
         if(idx == nums.length){
             ans.add(new ArrayList<Integer>(current));
