50d12aafc5df09675e1b52806b9f4bdc88ad3281
Manoj Kumar Jala <106336600+manojkumarjala-dev@users.noreply.github.com>
2025-07-12 19:45:53 -0400

    Time: 6 ms (76.16%), Space: 44.1 MB (39.05%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -12,18 +12,35 @@ class Solution {
         int[][] dp = new int[n+1][sum+1];
 
         // 0 sum 
-        dp[0][0] = 1;
+        for(int i =0;i<=n;i++) dp[i][0]=1;
+        //dp[0][0] = 1;
 
+
+        // for(int i = 1;i<=n;i++){
+        //     for(int j = 0;j<=sum;j++){
+        //         // if possible to add the count number of possibilities to include & exclude
+        //         if(nums[i-1]<=j){
+        //             dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+        //         }
+        //         else{
+        //         // since value is more than what we need copy the same sum exluding the ith index
+        //             dp[i][j] = dp[i-1][j];
+        //         }
+        //     }
+        //     //System.out.println(Arrays.toString(dp[i]));
+        // }
+
+        
 
         for(int i = 1;i<=n;i++){
             for(int j = 0;j<=sum;j++){
-                // if possible to add the count number of possibilities to include & exclude
-                if(nums[i-1]<=j){
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
-                }
-                else{
-                // since value is more than what we need copy the same sum exluding the ith index
-                    dp[i][j] = dp[i-1][j];
+                // special case of dealing with zeros
+                if (nums[i - 1] == 0 && j == 0) {
+                    dp[i][j] = dp[i - 1][j] * 2; //+0, -0
+                } else if (nums[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
             //System.out.println(Arrays.toString(dp[i]));
